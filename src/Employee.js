f2754194952f2328cf0b4da9d62f29221e929409
Hung Mai <hungmai.190801@gmail.com>
2024-01-22 22:12:29 +0700

    add h2 element

--- src/Employee.js
+++ src/Employee.js
@@ -3,7 +3,7 @@ export class Employee extends Component {
     render() {
         return (
             <div>
-                <h3>This is Employee Page</h3>
+                <h3>This is Employee Page design by MTH</h3>
             </div>
         )
     }
